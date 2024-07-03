b73e2da21d8e694940bc7073696d503f5dbf1b77
michaelcarroll33 <michaelcarroll788@gmail.com>
2024-07-02 21:52:06 -0400

    Return to a good starting point

--- src/App.js
+++ src/App.js
@@ -6,28 +6,22 @@ import { Switch } from '@mui/material';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import { ButtonGroup, Button } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 
 class App extends Component {
   render() {
     return (
-      <Router>
         <div>
-          <h2>Router Test</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              <li><Link to={'/'} className="nav-link">Home</Link></li>
-              <li><Link to={'/about'} className="nav-link">About</Link></li>
-              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            </ul>
-          </nav>
+          <h1>Michael Carroll</h1>
           <hr />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/contact' component={Contact} />
-          </Switch>
+          <ButtonGroup variant="text" aria-label="Basic button group">
+            <Button href="/">Home</Button>
+            <Button href="/about">About</Button>
+            <Button href="/contact">Contact</Button>
+          </ButtonGroup>
+          <hr />
         </div>
-      </Router>
     );
   }
 }
