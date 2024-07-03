f18ac9af26f3d511230b6b859af9c1553df8945a
michaelcarroll33 <michaelcarroll788@gmail.com>
2024-07-02 22:06:08 -0400

    Create Appbar base

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import './App.css';
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } 
 from 'react-router-dom';
-import { Switch } from '@mui/material';
+import { Switch, Typography } from '@mui/material';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import { ButtonGroup, Button } from '@mui/material';
-import { CircularProgress } from '@mui/material';
+import { Box, ButtonGroup, Button, IconButton, AppBar, Toolbar, Container } from '@mui/material';
+
 
 class App extends Component {
   render() {
     return (
         <div>
           <h1>Michael Carroll</h1>
+          <AppBar position="static">
+            <Container maxWidth="x1">
+              <Toolbar disableGutters>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="#app-bar-with-responsive-menu">
+
+                  </Typography>
+                  <Box s={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    // onClick={handleOpenNavMenu}
+                    color="inherit">
+                    </IconButton>
+                  </Box>
+              </Toolbar>
+            </Container>
+          </AppBar>
           <hr />
           <ButtonGroup variant="text" aria-label="Basic button group">
             <Button href="/">Home</Button>
